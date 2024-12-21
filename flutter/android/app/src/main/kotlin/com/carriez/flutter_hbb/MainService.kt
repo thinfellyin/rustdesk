e885d5f901188a3edf95d1677b455191073d34ca
thinfellyin <thinfell@gmail.com>
2024-12-21 16:52:25 +0800

    Update MainService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -543,8 +543,7 @@ class MainService : Service() {
                     virtualDisplay = mp.createVirtualDisplay(
                         "RustDeskVD",
                         SCREEN_INFO.width, SCREEN_INFO.height, SCREEN_INFO.dpi,
-                        DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR or
-                        DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                        DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
                         s, null, null
                     )
                 }
